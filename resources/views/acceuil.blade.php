83a3c138110ccccfbef96ce8d899cd1ebd6e664e
Prunelle <prunelledalmeida500@gmail.com>
2025-07-14 19:33:56 +0200

    ajout de la page d'acceuil stylisé

---
+++ resources/views/acceuil.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil eat&drink</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <!--header-->
+   <header>
+      <nav class="navbar">
+      <div class="logo">  
+        <img src="{{ asset('images/edlogo1.png') }}" alt="un logo" id="logo" width="300">
+      </div>
+       <ul class="nav-links">
+        <li><a href="#" class="btn ">S'inscrire</a></li>
+        <li><a href="#" class="btn ">View stand</a></li>
+      </ul>
+    </nav>
+  </header>
+      <!--main content-->
+    <main>
+        <div class="global">
+            <div class="description">
+                <h1>WELCOME TO EAT&DRINK</h1>
+                <p>Venez découvrir une variété de stands proposant des plats délicieux et des spécialités locales.Que vous soyez amateur de sucré ou de salé, il y en a pour tous les goûts !!</p><br><br> 
+                <p> Plongez au coeur d'une experience sensorielle inoubliable.Notre évènement vous invite à explorer un univers de saveurs et de créations, où chaque stand raconte une histoire et chaque dégustation est une invitation au voyage. Laissez-vous surprendre </p>
+            
+            </div>
+
+        </div>
+    </main>
+      
+</body>
+</html>
