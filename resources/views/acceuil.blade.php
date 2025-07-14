031817cf25714649b027b1d988983278154848ee
Prunelle <prunelledalmeida500@gmail.com>
2025-07-14 21:18:53 +0200

    modificztion de la page d'acceuil stylisée

--- resources/views/acceuil.blade.php
+++ resources/views/acceuil.blade.php
@@ -28,9 +28,32 @@
                 <p> Plongez au coeur d'une experience sensorielle inoubliable.Notre évènement vous invite à explorer un univers de saveurs et de créations, où chaque stand raconte une histoire et chaque dégustation est une invitation au voyage. Laissez-vous surprendre </p>
             
             </div>
+      <section class="gallery">
+    <img src="{{ asset('images/ex11.jpg') }}" alt="Cupcake">
+    <img src="{{ asset('images/ex2.jpg') }}" alt="Bonbons">
+    <img src="{{ asset('images/ex3.jpg') }}" alt="Glace">
+    <img src="{{ asset('images/ex4.jpg') }}" alt="Food Truck">
+    <img src="{{ asset('images/ex5.jpg') }}" alt="Crêpes">
+    <img src="{{ asset('images/ex6.jpg') }}" alt="Popcorn">
+    <img src="{{ asset('images/ex7.jpg') }}" alt="Popcorn">
+    <img src="{{ asset('images/ex8.jpg') }}" alt="Popcorn">
+    <img src="{{ asset('images/ex12.jpg') }}" alt="pizza">
+    <img src="{{ asset ('images/ex13.jpg') }}" alt="la">
+  </section>
+</div>
+</main>
+  
 
-        </div>
-    </main>
-      
+
+  <footer>
+    <div class="footer-content">
+      <p>&copy; 2025 Fête des Gourmandises. Tous droits réservés.</p>
+      <div class="socials">
+        <a href="#">Instagram</a>
+        <a href="#">Facebook</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  </footer> 
 </body>
 </html>
