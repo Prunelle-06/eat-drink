6f0823270de7e9349bb1c3ad01f7f29c77fca156
Fayad ROUFAI <fayadroufai241@gmail.com>
2025-07-20 21:50:58 +0200

    modification design de la page de connexion

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,48 +13,50 @@
 </style>
 <body>
     @include('layouts.header')
-    <section class="login-form">
-        <h1>Connexion</h1>
-        
-        @if($errors->any())
-            <div style="color: red; background: lightred; padding: 16px">
-                {{ $errors->first() }}
-            </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-                    
-            <div class="form-group">
-                <input type="email" 
-                    name="email" 
-                    placeholder="Adresse email" 
-                    value="{{ old('email') }}"
-                    required
-                    autofocus>
-            </div>
+    <section style="display: flex; justify-content: center;">
+        <section class="login-form">
+            <h1>Connexion</h1>
             
-            <div class="form-group">
-                <input type="password" 
-                    name="password" 
-                    placeholder="Mot de passe" 
-                    required>
-            </div>
+            @if($errors->any())
+                <div style="color: red; background: lightred; padding: 16px">
+                    {{ $errors->first() }}
+                </div>
+            @endif
 
-            <div class="form-group remember">
-                <input type="checkbox" name="remember" id="remember">
-                <label for="remember">Se souvenir de moi</label>
-            </div>
-            
-            <button type="submit" class="submit-btn">Connexion</button>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                        
+                <div class="form-group">
+                    <input type="email" 
+                        name="email" 
+                        placeholder="Adresse email" 
+                        value="{{ old('email') }}"
+                        required
+                        autofocus>
+                </div>
+                
+                <div class="form-group">
+                    <input type="password" 
+                        name="password" 
+                        placeholder="Mot de passe" 
+                        required>
+                </div>
 
-            <div class="links">
-                <a href="">Mot de passe oublié ?</a>
-                @if(Route::has('register'))
-                    <a href="{{ route('register') }}">Créer un compte</a>
-                @endif
-            </div>
-        </form>
+                <div class="form-group remember">
+                    <input type="checkbox" name="remember" id="remember">
+                    <label for="remember">Se souvenir de moi</label>
+                </div>
+                
+                <button type="submit" class="submit-btn">Connexion</button>
+
+                <div class="links">
+                    <a href="">Mot de passe oublié ?</a>
+                    @if(Route::has('register'))
+                        <a href="{{ route('register') }}">Créer un compte</a>
+                    @endif
+                </div>
+            </form>
+        </section>
     </section>      
   
     @include('layouts.footer')
