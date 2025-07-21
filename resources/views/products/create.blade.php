a1322fff958247a1392df28a656935b5b308c52e
Fayad ROUFAI <fayadroufai241@gmail.com>
2025-07-21 18:28:27 +0200

    Ajout de produit en base de données(creation)

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/product-create.css') }}">
+    <title>Formulaire Produit</title>
+</head>
+<body>
+    <div class="form-container">
+        <h2>Ajouter un produit</h2>
+        <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="nom">Nom du produit</label>
+                <input type="text" id="nom" name="nom_produit" value="{{ old('nom_produit') }}" required>
+            </div>
+            @error('nom_produit') 
+                <p>{{ $message }}</p>
+            @enderror
+
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea id="description" name="description" value="{{ old('description') }}" rows="4" required></textarea>
+            </div>
+            @error('description') 
+                <p>{{ $message }}</p>
+            @enderror
+
+            <div class="form-group">
+                <label for="prix">Prix</label>
+                <input type="text" id="prix" name="prix" value="{{ old('prix') }}" required>
+            </div>
+            @error('prix') 
+                <p>{{ $message }}</p>
+            @enderror
+
+            <div class="form-group">
+                <label for="photo">Photo</label>
+                <div class="custom-file">
+                    <input type="file" id="photo" name="photo" value="{{ old('photo') }}" accept="image/*" required>
+                    <span id="file-name">Aucune image sélectionnée</span>
+                </div>
+            </div>
+            @error('photo') 
+                <p>{{ $message }}</p>
+            @enderror
+
+            <button type="submit">Soumettre</button>
+        </form>
+    </div>
+
+
+
+ 
+    
+
+    <script>
+        document.getElementById('photo').addEventListener('change', function () {
+            const fileName = this.files[0]?.name || "Aucune image sélectionnée";
+            document.getElementById('file-name').textContent = fileName;
+        });
+    </script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InscriptionController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\ProductController;
 
 // Route page acceuil
 Route::get('/', function () {
@@ -24,10 +25,17 @@ Route::get('/attente', function () {
     return view('pending');
 })->middleware(['auth', 'can:acces-attente']);
  
-// Route ADMIN
+// Routes ADMIN
 Route::prefix('admin')->middleware(['auth', 'is_admin', 'is_pending'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
     Route::post('/users/{id}/approve', [DashboardController::class, 'approve'])->name('admin.approve');
     Route::post('/users/{id}/reject', [DashboardController::class, 'reject'])->name('admin.reject');
+});
+
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/products', 'index')->name('products.index');
+    Route::get('/products/create', 'create')->name('products.create');
+    Route::post('/products', 'store')->name('products.store');
 });
 
